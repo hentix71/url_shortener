9abc4acea6b19422f60eea8e8273783a3cb31f87
Hentix <iu3071pc@gmail.com>
2026-06-06 18:57:59 +0545

    feat: update url model

--- url/models.py
+++ url/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from account.models import User
-
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 class ShortURL(models.Model):
+
+    class LifeDurationChoices(models.TextChoices):
+        ONE_HOUR = '1h', _('1 Hour')
+        ONE_DAY = '1d', _('1 Day')
+        ONE_WEEK = '1w', _('1 Week')
+        NEVER = 'never', _('Never') 
+
     user = models.ForeignKey(
         User, 
         on_delete=models.CASCADE, 
@@ -12,16 +20,41 @@ class ShortURL(models.Model):
     original_url = models.URLField()
 
     short_code = models.CharField(
-        max_length=10, 
+        max_length=7, 
         unique=True
     )
 
     click_count = models.PositiveIntegerField(
         default=0,
-        help_text="Number of times the short URL has been accessed."
     )
     
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+
+    life_time = models.CharField(
+        max_length=10,
+        default = LifeDurationChoices.NEVER,
+        choices= LifeDurationChoices.choices,
+        verbose_name = _("URL Duration")
+    )
+
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name = _("Active Status")
+    )
+
+    expires_at = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+
+    @property
+    def is_expired(self):
+        return (
+            self.expires_at is not None and self.expires_at <= timezone.now()
+        )
 
     def __str__(self):
         return f"{self.short_code} -> {self.original_url}"
