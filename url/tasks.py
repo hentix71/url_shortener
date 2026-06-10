c62baeb0e2d3d85db6cca103b717fdef1e2a5991
Hentix <iu3071pc@gmail.com>
2026-06-10 17:16:00 +0545

    fix: updated the tasks.py to use default cache

--- url/tasks.py
+++ url/tasks.py
@@ -1,26 +1,17 @@
 from celery import shared_task
 from django.db.models import F
-
-
 from .models import ShortURL
-from .services.redis_service import redis_client
+from django.core.cache import cache
 
 @shared_task
 def sync_click_counts():
-
-    print("starting syncing click counts...")
-    for key in redis_client.scan_iter("clicks:*"):
-        short_code = key.split(":",1 )[1]
-
-        click_count = int(redis_client.get(key) or 0)
-        
-        if click_count <= 0:
+    client = cache.client.get_client()
+    for key in client.scan_iter("clicks:*"):
+        short_code = key.split(":", 1)[1]
+        click_count = client.get(key)
+        if not click_count or int(click_count) <= 0:
             continue
-        
-        ShortURL.objects.filter(
-            short_code=short_code
-            ).update(
-                click_count=F('click_count') + click_count
-                )
-        redis_client.delete(key)    
-    print("Finished syncing click counts.")
+        ShortURL.objects.filter(short_code=short_code).update(
+            click_count=F('click_count') + int(click_count)
+        )
+        client.delete(key)
